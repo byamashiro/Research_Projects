48e65a0592c257255213d55074f1600420611fe6
Bryan Yamashiro <byamashiro09@gmail.com>
2017-07-06 23:19:14 -1000

    cdf merge columns

--- radio_remastered.py
+++ radio_remastered.py
@@ -1,6 +1,8 @@
 from spacepy import pycdf
+import pandas as pd
 
 cdf = pycdf.CDF('wi_h1_wav_20120307_v01.cdf')
+
 
 time_rb = []
 
@@ -20,5 +22,21 @@ voltage_rad1 = []
 for i in cdf['E_VOLTAGE_RAD1']:
 	voltage_rad1.append(i)
 
-print(voltage_rad1)
+#print(voltage_rad1)
+
+
+data_time = pd.DataFrame(time_rb)
+freq_panda = pd.DataFrame(freq_rad)
+freq_panda.columns = ['freq']
+data_volt = pd.DataFrame(voltage_rad1)
+data_volt.columns = freq_panda['freq']
+#full_data = pd.concat([data_time, data_volt], axis=1)
+
+'''
+data_full = pd.DataFrame({
+	'datetime': time_rb,
+	'interesting': voltage_rad1
+})
+'''
+
 
