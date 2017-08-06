db76527cd0526a4382ff88050649faa3e43afa4b
Bryan Yamashiro <byamashiro09@gmail.com>
2017-08-05 22:56:04 -1000

    old bash scripts for downloads

---
+++ Scripts/deprecated_scripts/full_forbushdates.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+python event.py 2011 06 16
+python event.py 2011 06 24
+python event.py 2011 07 10
+python event.py 2011 07 19
+python event.py 2011 08 06
+python event.py 2011 09 09
+python event.py 2011 09 17
+python event.py 2011 09 26
+python event.py 2011 10 25
+python event.py 2011 11 28
+echo '2011 FD Completed (1/6)'
+
+python event.py 2012 01 22
+python event.py 2012 02 27
+python event.py 2012 03 08
+python event.py 2012 04 06
+python event.py 2012 04 20
+python event.py 2012 04 25
+python event.py 2012 05 30
+python event.py 2012 06 17
+python event.py 2012 07 04
+python event.py 2012 07 15
+python event.py 2012 09 04
+python event.py 2012 09 30
+python event.py 2012 10 07
+python event.py 2012 11 13
+python event.py 2012 11 24
+echo '2012 FD Completed (2/6)'
+
+
+python event.py 2013 01 18
+python event.py 2013 01 25
+python event.py 2013 03 16
+python event.py 2013 04 13
+python event.py 2013 06 22
+python event.py 2013 07 09
+python event.py 2013 08 03
+python event.py 2013 08 22
+python event.py 2013 09 02
+python event.py 2013 10 01
+python event.py 2013 10 29
+python event.py 2013 11 10
+python event.py 2013 11 30
+python event.py 2013 12 07
+python event.py 2013 12 14
+python event.py 2013 12 31
+echo '2013 FD Completed (3/6)'
+
+python event.py 2014 01 09
+python event.py 2014 02 15
+python event.py 2014 02 20
+python event.py 2014 02 27
+python event.py 2014 04 05
+python event.py 2014 04 18
+python event.py 2014 05 22
+python event.py 2014 06 07
+python event.py 2014 06 15
+python event.py 2014 09 04
+python event.py 2014 09 12
+python event.py 2014 11 30
+python event.py 2014 12 21
+echo '2014 FD Completed (4/6)'
+
+
+python event.py 2015 03 16
+python event.py 2015 04 09
+python event.py 2015 05 05
+python event.py 2015 06 13
+python event.py 2015 06 22
+python event.py 2015 07 13
+python event.py 2015 08 15
+python event.py 2015 08 26
+python event.py 2015 09 07
+python event.py 2015 11 06
+python event.py 2015 12 06
+python event.py 2015 12 31
+echo '2015 FD Completed (5/6)'
+
+python event.py 2016 03 05
+python event.py 2016 03 24
+python event.py 2016 04 21
+echo '2016 FD Completed (6/6)'
