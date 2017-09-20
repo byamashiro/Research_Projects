562167926e6e4e4618cefbfc81de96bdea397ef6
Bryan Yamashiro <byamashiro09@gmail.com>
2017-09-20 13:10:03 -1000

    added a full plot feature for omni script

--- Scripts/pandas_test_omni.py
+++ Scripts/pandas_test_omni.py
@@ -19,7 +19,8 @@ plt.close("all")
 
 data_directory = '/Users/bryanyamashiro/Documents/Research_Projects/Data'
 save_option = 'yes' # saves the data files
-save_plot_option = 'no' # saves the plots
+save_plot_option = 'yes' # saves the plots
+long_plot_option = 'yes'
 
 
 
@@ -84,7 +85,7 @@ while True: # energy_bin != 'done':
 			elif int(option_bin) < 8:
 				option_bin_set.add(option_bin)
 	
-				if len(option_bin_set) > 4:
+				if len(option_bin_set) > 4 and long_plot_option == 'no':
 					print('SELECTION ERROR: Only 4 datasets are allowed per canvas.')
 					sys.exit(0)
 	
@@ -1141,7 +1142,10 @@ def applyPlotStyle():
 
 
 if length_data > 1:
-	f, axes = plt.subplots(nrows=length_data, ncols=1, sharex=True, figsize=(10, 6))
+	if long_plot_option != 'yes':
+		f, axes = plt.subplots(nrows=length_data, ncols=1, sharex=True, figsize=(10, 6))
+	elif long_plot_option == 'yes':
+		f, axes = plt.subplots(nrows=length_data, ncols=1, sharex=True, figsize=(10, 12))
 
 if length_data == 1:
 	length_data_list[0] = 0,0
@@ -1336,8 +1340,11 @@ plt.subplots_adjust(wspace = 0, hspace = 0, top=0.91)
 if event_option == 'yes':
 	plt.savefig(f'xflare_events/omni_test_{event_date}.png', format='png', dpi=900)
 
-if save_plot_option == 'yes':
+if save_plot_option == 'yes' and long_plot_option != 'yes':
 	plt.savefig(f'xflare_events/omni_test_{event_date}.png', format='png', dpi=900)
+
+elif save_plot_option == 'yes' and long_plot_option == 'yes':
+	plt.savefig(f'full_omni_plots/omni_test_{event_date}.png', format='png', dpi=900)
 
 elif save_plot_option != 'yes':
 	plt.show()
