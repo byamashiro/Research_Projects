a59c9667e23420bf25ec445d72d70a7b958c348b
Bryan Yamashiro <byamashiro09@gmail.com>
2017-09-28 13:24:52 -1000

    hdf reader

---
+++ Scripts/hdf_test.py
@@ -0,0 +1,14 @@
+import h5py
+import numpy as np
+import pandas as pd
+
+def keys(f):
+    return [key for key in f.keys()]
+'''
+f = h5py.File('mag_data_16sec_2496.hdf', 'r')
+print(keys(f))
+f.close()
+'''
+with pd.HDFStore('mag_data_16sec_2496.hdf',  mode='r') as newstore:
+    df_restored = newstore.select('df')
+
